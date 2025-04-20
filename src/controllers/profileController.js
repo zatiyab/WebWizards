5f95ac0b0d4ba02101b88bdc4e1fe7ec5133e6a6
Ali <aliatiyab.husain@gmail.com>
2025-04-20 10:45:24 +0530

    Edit Profile changes

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -38,7 +38,7 @@ const editProfile = async (req, res) => {
     try {
         data = req.body
         console.log(data)
-        const result = await db.query("UPDATE users SET email=$1, password=$2, name=$3, batch=$4, branch=$5, job_title=$6, location=$7 WHERE user_id = $8;", [data.email, data.password, data.name, data.batch, data.branch, data.job, data.location])
+        const result = await db.query("UPDATE users SET email=$1, password=$2, name=$3, batch=$4, branch=$5, job_title=$6, location=$7 WHERE user_id = $8;", [data.email, data.password, data.name, data.batch, data.branch, data.job, data.location, req.session.user.user_id])
         res.render('editProfile.ejs')
     } catch (error) {
         console.error('Error fetching directory:', error);
